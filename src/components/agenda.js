d647d690c7c4c4c595829dffbf56eb4ddc2c5842
nadir <nadir.atif.94@gmail.com>
2022-11-20 17:05:03 +0100

    add agenda mock up

---
+++ src/components/agenda.js
@@ -0,0 +1,21 @@
+export const agendaData = [
+    {
+        id: 1,
+        title: 'Keynote',
+        speaker: 'John Brand',
+        time: '2022-11-20',
+    },
+    {
+        id: 2,
+        title: 'React Native',
+        speaker: 'Jane Doe',
+        time: '2022-11-29',
+    },
+    {
+        id: 3,
+        title: 'React',
+        speaker: 'John Smith',
+        time: '2022-11-15',
+    },
+];
+
